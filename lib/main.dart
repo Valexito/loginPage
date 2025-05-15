37002474f8cfd296dd49b0367dcfd9b9c9ef6a08
Valexito <david.argueta@guatestar.org>
2025-05-15 11:13:12 -0600

    Running with erros in Gradle version

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:google_login/pages/login_page.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
 
 void main() {
   runApp(const MyApp());
